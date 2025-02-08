1b18bf1e312d64736eec6a72234762fc7a2a0dff
Kai Lycaon <92839483+ShadowyLycan990@users.noreply.github.com>
2025-02-09 01:00:57 +0700

    Update BoardConfig.mk

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -10,10 +10,10 @@ DEVICE_PATH := device/samsung/gta3xl
 OVERRIDE_TARGET_FLATTEN_APEX := true
 
 # Architecture
-TARGET_ARCH := arm
+TARGET_ARCH := arm64
 TARGET_ARCH_VARIANT := armv8-a
-TARGET_CPU_ABI := armeabi-v7a
-TARGET_CPU_ABI2 := armeabi
+TARGET_CPU_ABI := arm64-v8a
+TARGET_CPU_ABI2 := armeabi-v7a
 TARGET_CPU_VARIANT := generic
 TARGET_CPU_VARIANT_RUNTIME := cortex-a53
 
